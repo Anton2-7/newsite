5b8173c8d8d25b700d76c83d5c0cee3c01d9cd40
Anton2-7 <60310157+Anton2-7@users.noreply.github.com>
2021-11-08 15:20:38 +0500

    New

--- js/scrips.js
+++ js/scrips.js
@@ -1,2 +1,7 @@
-$(window).load(function() { $('.load').fadeOut('slow'); });
-
+$(document).ready(function() {
+	$('.menu-burger__header').click(function(){
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+	});
+});
